5255d4191e5fd48354ab8af7a674d70a857d33f6
mvaldes <mvaldes.revilla@gmail.com>
2025-02-24 23:15:47 -0600

    initial commit

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      flake = {
+        templates = {
+          iac = {
+            description = "Packages for IaC";
+            path = ./template/iac;
+          };
+        };
+      };
+      systems = ["x86_64-linux" "aarch64-linux"];
+      perSystem = {
+        config,
+        system,
+        pkgs,
+        ...
+      }: let
+        inherit (pkgs) jq mkShell;
+      in {
+        devShells = {
+          default = mkShell {
+            buildInputs = [jq];
+            shellHook = ''
+              echo "Pick a flake to build"
+            '';
+          };
+        };
+      };
+    };
+}
---
+++ template/iac/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "IaC Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux"];
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        inherit (pkgs) mkShell;
+      in {
+        devShells = {
+          default = mkShell {
+            buildInputs = with pkgs; [terraform tfsec tflint];
+            shellHook = ''
+              Commands available:
+              - terraform
+              - tfsec
+              - tflint
+            '';
+          };
+        };
+      };
+    };
+}
