63e6fda919a112c970095228743c434b164e446e
mvaldes <mvaldes.revilla@gmail.com>
2025-03-26 23:41:30 -0500

    adding mac to all flakes

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
           };
         };
       };
-      systems = ["x86_64-linux" "aarch64-linux"];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
       perSystem = {
         config,
         system,
